81dd595f64c95e3e303d31f397f5c20e152d395b
Kattorzeh <mariorobresroyuela@gmail.com>
2024-03-20 10:06:21 +0100

    add initial workflow&template, created actions classes and main method aws-autodeploy

---
+++ .github/ISSUE_TEMPLATE/test.md
@@ -0,0 +1,26 @@
+---
+name: EC2 Instance
+description: This issue will create and configure an EC2 instance.
+title: "EC2 Template"
+labels: ["state-pending", "action-validate"]
+assignees: ''
+----
+
+---
+<!--
+
+This template is used to configure and deploy an EC2 instance.
+
+An example template with the minimum fields is shown below.
+
+:name: "ec2_mario"
+:email: "example@gmail.com"
+:instance_type: "t2.micro"
+
+-->
+
+:name: 
+:email: 
+:instance_type: 
+
+...
---
+++ src/aws_autodeploy.rb
@@ -0,0 +1,26 @@
+# Global Objects
+# Logger
+LOG_COMP = 'MAIN'
+
+logger = Logger.new(STDOUT)
+Log.logger = logger
+Log.level  = 3
+Log.info(LOG_COMP, 'Starting AWS-Autodeploy')
+
+# Action tag
+action_tag = ARGV[0].to_s
+
+
+
+# Code:
+
+# Generate action from issue
+action = Action.for(action_tag)
+
+# Execute action
+Log.debug(LOG_COMP, "Executing action '#{action_tag}'")
+action.execute(self)
+
+# Report results
+Log.debug(LOG_COMP, "Reporting action '#{action_tag}'")
+action.report(self)
