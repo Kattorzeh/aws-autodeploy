394a4edb7785b7c00a64c95356a248b00fbcd194
Kattorzeh <mariorobresroyuela@gmail.com>
2024-03-23 19:12:16 +0100

    test issue pass

--- src/actions/validate_action.rb
+++ src/actions/validate_action.rb
@@ -3,7 +3,28 @@ require 'actions/action'
 class ValidateAction < Action
     attr_reader :success
 
-    def execute()
+    def execute(issue)
         Log.info(LOG_COMP, "Validating template")
+        issue_number = issue['number']
+        issue_title = issue['title']
+        issue_body = issue['body']
+
+        comments = issue['comments']
+        labels = issue['labels']
+        state = issue['state']
+        created_at = issue['created_at']
+        updated_at = issue['updated_at']
+    
+        Log.info(LOG_COMP, "issue Num: #{issue_number}")
+        Log.info(LOG_COMP, "Issue Title: #{issue_title}")
+        Log.info(LOG_COMP, "Issue Body: #{issue_body}")
+
+        Log.info(LOG_COMP, "Comments: #{comments}")
+        Log.info(LOG_COMP, "Labels: #{labels}")
+        Log.info(LOG_COMP, "Assigned to: #{assignees}")
+        Log.info(LOG_COMP, "State: #{state}")
+        Log.info(LOG_COMP, "Creation Date: #{created_at}")
+        Log.info(LOG_COMP, "Last Updated Date: #{updated_at}")
+
     end
 end
--- src/aws_autodeploy.rb
+++ src/aws_autodeploy.rb
@@ -4,9 +4,10 @@ $LOAD_PATH << '../src'
 require 'logger'
 require 'tools/log'
 require_relative 'actions/action'
+require 'json'
 require 'octokit'
 
-# Global Objects
+# Global Objects:
 
 # Logger
 LOG_COMP = 'MAIN'
@@ -19,23 +20,23 @@ Log.info(LOG_COMP, 'Starting AWS-Autodeploy')
 action_tag = ARGV[0].to_s
 Log.debug(LOG_COMP, "Action tag: #{action_tag}")
 
+# Issue from JSON
+issue_json = ARGV[0]
+issue = JSON.parse(issue_json)
+
 # Code:
 
-# Generate action from issue
+# Generate action from issue tag
 action = Action.for(action_tag)
 
 # Execute action
 Log.debug(LOG_COMP, "Executing action '#{action_tag}'")
-action.execute()
+action.execute(issue)
 
 # Report results
 Log.debug(LOG_COMP, "Reporting action '#{action_tag}'")
 action.report()
 
 # Add description field in the Issue
-
-# Crear un cliente Octokit usando el token de acceso de GitHub Actions
 client = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
-
-# Agregar un comentario a la issue actual
 client.add_comment(ENV['GITHUB_REPOSITORY'], ENV['GITHUB_EVENT_NUMBER'], "Esto es un ejemplo de comentario desde el script aws_autodeploy.rb")
