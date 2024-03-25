6caddeadba30de9b92e453d411919f07c5a12423
Kattorzeh <mariorobresroyuela@gmail.com>
2024-03-25 18:39:27 +0100

    test multiple data from a key in issue temp v0.7.1

--- src/tools/issue_params.rb
+++ src/tools/issue_params.rb
@@ -16,6 +16,9 @@ class IssueParams
         parsed_input[key] = values
       end
     end
+    parsed_input.each do |key, value|
+      puts "#{key}: #{value.join(',')}"
+    end
     return parsed_input
   end
 
