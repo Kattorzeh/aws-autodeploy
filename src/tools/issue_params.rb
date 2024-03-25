d7ce772f919ac133a5f93e37e849393d7e2a71ea
Kattorzeh <mariorobresroyuela@gmail.com>
2024-03-25 18:28:14 +0100

    test multiple data from a key in issue temp v0.6.1

--- src/tools/issue_params.rb
+++ src/tools/issue_params.rb
@@ -6,21 +6,20 @@ class IssueParams
   
     def get_params()
       params = {}
-    
+
       matches = @body.scan(/:(\w+):\s*["']?(.*?)["']?(?:,|$)/)
       matches.each do |match|
         param = match[0]
-        value = match[1].strip.split(',')  #
-
+        value = match[1].split(',')
+        
         # Quit \r 
         value.map!(&:chomp)
-    
-        # +1 value x key -> converts it into an array (if it sin't it)
+
         if params.key?(param.to_sym)
           if params[param.to_sym].is_a?(Array)
-            params[param.to_sym] << value
+            params[param.to_sym] += value
           else
-            params[param.to_sym] = [params[param.to_sym], value]
+            params[param.to_sym] = [params[param.to_sym]] + value
           end
         else
           params[param.to_sym] = value
