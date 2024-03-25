3b0db2f63962fb9dd76116dcf454c014d92640de
Kattorzeh <mariorobresroyuela@gmail.com>
2024-03-25 23:38:45 +0100

    test multiple validation v0.8.2

--- src/templates/ec2_validator.rb
+++ src/templates/ec2_validator.rb
@@ -2,75 +2,46 @@ require 'aws-sdk-ec2'
 require_relative 'validate_template'
 
 module EC2ValidatorMethods
+  def validate_ec2_instance_type(instance_type, aws_ec2_client)
+    errors = []
+    if instance_type.nil?
+      errors << "EC2 instance type is not specified."
+    else
+      response_type = aws_ec2_client.describe_instance_type_offerings(
+        filters: [{ name: 'instance-type', values: [instance_type] }]
+      )
+      if response_type.instance_type_offerings.empty?
+        errors << "The instance type '#{instance_type}' is not valid."
+      end
+    end
+    errors
+  end
 
-    def initialize
-        super
+  def validate_ec2_ami(ami_id, aws_ec2_client)
+    errors = []
+    if ami_id.nil?
+      errors << "EC2 AMI is not specified."
+    else
+      begin
+        response_bad_ami = aws_ec2_client.describe_images(image_ids: [ami_id])
+      rescue Aws::EC2::Errors::InvalidAMIIDNotFound => e
+        errors << "AMI '#{ami_id}' not found."
+      end
     end
-
-    def validate_ec2_instances(values)
-      validate_numerical_values(values, "ec2_instances")
-    end
-  
-    def validate_ec2_name(values)
-      validate_strings(values, "ec2_name")
-    end
-  
-    def validate_ec2_ami_os(values)
-      validate_ami_os(values, "ec2_ami_os")
-    end
-  
-    def validate_ec2_tags(values)
-      validate_strings(values, "ec2_tags")
-    end
-
-    def self.validate_ec2_instance_type(instance_type, aws_ec2_client)
-        errors = []
-        if instance_type.nil?
-            errors << "EC2 instance type is not specified."
-        else
-            response_type = aws_ec2_client.describe_instance_type_offerings(
-                filters: [{ name: 'instance-type', values: [instance_type] }]
-            )
-            if response_type.instance_type_offerings.empty?
-                errors << "The instance type '#{instance_type}' is not valid."
-            end
-        end
-        errors
-    end
-    
-    def self.validate_ec2_ami(ami_id, aws_ec2_client)
-        errors = []
-        if ami_id.nil?
-            errors << "EC2 AMI is not specified."
-        else
-            begin
-                response_bad_ami = aws_ec2_client.describe_images(image_ids: [ami_id])
-            rescue Aws::EC2::Errors::InvalidAMIIDNotFound => e
-                errors << "AMI '#{ami_id}' not found."
-            end
-        end
-        errors
-    end
-  
-    private
-  
-    def validate_strings(values, key)
-      values.map { |value| "Invalid #{key}: #{value}" unless value.match?(/\A[\w\d\-]+\z/) }.compact
-    end
-  
-    def validate_numerical_values(values, key)
-      values.map { |value| "Invalid #{key}: #{value}" unless value.to_i.between?(0, 5) }.compact
-    end
-  
-    def validate_ami_os(values, key)
-      values.map { |value| "Invalid #{key}: #{value}" unless ['windows', 'linux'].include?(value.downcase) }.compact
-    end
+    errors
+  end
 end
 
 class EC2Validator < ValidateTemplate
-    include EC2ValidatorMethods
+  include EC2ValidatorMethods
 
-    def initialize
-        super
-    end
+  def initialize
+    super
+  end
+
+  def validate(params)
+    errors = super
+    errors.each { |error| puts error }
+    errors
+  end
 end
--- src/templates/validate_template.rb
+++ src/templates/validate_template.rb
@@ -1,26 +1,27 @@
 class ValidateTemplate
-    LOG_COMP = 'VAL_TEMP'
-  
-    def initialize
-      Aws.config.update({
-        credentials: Aws::Credentials.new(ENV['ACCESS_KEY_ID'], ENV['SECRET_ACCESS_KEY']),
-        region: ENV['REGION']
-      })
-      @aws_ec2_client = Aws::EC2::Client.new
-    end
-  
-    def validate(params)
-      errors = []
-      params.each do |key, values|
+  LOG_COMP = 'VAL_TEMP'
+
+  def initialize
+    Aws.config.update({
+      credentials: Aws::Credentials.new(ENV['ACCESS_KEY_ID'], ENV['SECRET_ACCESS_KEY']),
+      region: ENV['REGION']
+    })
+    @aws_ec2_client = Aws::EC2::Client.new
+  end
+
+  def validate(params)
+    errors = []
+    params.each do |key, values|
+      if key.to_s.start_with?('ec2_')
         validation_method = "validate_#{key}".to_sym
         if respond_to?(validation_method, true)
-          errors.concat(send(validation_method, values))
+          errors.concat(send(validation_method, values, @aws_ec2_client))
         else
           puts "No validation method found for key: #{key}"
         end
       end
-      errors
     end
-    
+    errors
+  end
 end
   
